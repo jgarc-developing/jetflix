f949c23cd6174d68d6a869aec915f191c4578cff
Carlito <rody.castillo.tacuri@gmail.com>
2021-12-08 00:22:37 -0500

    chore: add simple navbar and routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = () => {
     return (
-        <div>
+        <div className="container">
             <h1>home</h1>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <>
-            
-        </>
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <a href="/" className="navbar-brand mb-0 h1">Jetflix</a>
+
+                <ul className="navbar-nav">
+                    <button className="nav-item btn btn-danger">
+                        <a className="nav-link active" aria-current="page" href="/login">Login</a>
+                    </button>
+                </ul>
+            </div>
+        </nav>
     )
 }
