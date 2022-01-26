1031046c4e077616385057ff95ca46823c323247
Carlito <rody.castillo.tacuri@gmail.com>
2022-01-25 23:50:21 -0500

    [chore]: update txt validation register

--- packages/react-jetflix/src/pages/Register.js
+++ packages/react-jetflix/src/pages/Register.js
@@ -10,13 +10,13 @@ export const Register = () => {
   const [email, setEmail] = useState("");
   const validationSchema = yup.object({
     email: yup
-      .string("Enter your email")
-      .email("Enter a valid email")
-      .required("Email is required"),
+      .string("Ingrese su correo")
+      .email("Ingrese un correo válido")
+      .required("El correo es obligatorio"),
     password: yup
-      .string("Enter your password")
-      .min(8, "Password should be of minimum 8 characters length")
-      .required("Password is required"),
+      .string("Ingrese su contraseña")
+      .min(8, "La contraseña debe tener más de 8 carácteres")
+      .required("La contraseña es obligatorio"),
   });
 
   const formik = useFormik({
